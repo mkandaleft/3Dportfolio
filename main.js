696e54201ba7769b0183510be34e73c83dbef648
mkandaleft <markandaleft@hotmail.com>
2024-03-18 11:54:03 -0400

    Added Pointer Lock

--- main.js
+++ main.js
@@ -39,18 +39,51 @@ class InputController {
     this.target_.addEventListener('mouseup', (e) => this.onMouseUp_(e), false);
     this.target_.addEventListener('keydown', (e) => this.onKeyDown_(e), false);
     this.target_.addEventListener('keyup', (e) => this.onKeyUp_(e), false);
-  }
+    
+    // document.addEventListener('pointerlockchange', this.lockChangeAlert.bind(this), false);
+    // document.addEventListener('mozpointerlockchange', this.lockChangeAlert.bind(this), false);
+
+    this.target_.addEventListener('click', (e) => {
+      // Request pointer lock
+      document.body.requestPointerLock = document.body.requestPointerLock || document.body.mozRequestPointerLock;
+      document.body.requestPointerLock();
+    }, false);
+    
+    }
+    /*
+    lockChangeAlert() {
+      const ptLock = document.pointerLockElement === this.target_;
+      const mzPoint = document.mozPointerLockElement === this.target_;
+      console.log(ptLock);
+      console.log(mzPoint);
+      setTimeout(() => {  // Delay the execution of the method
+        // Check if the document is in pointer lock mode
+        if (document.pointerLockElement === this.target_ || document.mozPointerLockElement === this.target_) {
+        console.log('The pointer lock status is now locked');
+        // Do any additional pointer lock setup here
+        this.target_.addEventListener('mousemove', this.onMouseMoveBound_, false);
+        } else {
+        console.log('The pointer lock status is now unlocked');  
+        // Do any additional pointer lock cleanup here
+        this.target_.removeEventListener('mousemove', this.onMouseMoveBound_, false);
+        }
+      }, 0);
+    }
+    */
 
   onMouseMove_(e) {
-    this.current_.mouseX = e.pageX - window.innerWidth / 2;
-    this.current_.mouseY = e.pageY - window.innerHeight / 2;
+    // Calculate how much the mouse has moved
+    const movementX = e.movementX || e.mozMovementX || 0;
+    const movementY = e.movementY || e.mozMovementY || 0;
 
-    if (this.previous_ === null) {
-      this.previous_ = {...this.current_};
-    }
+    // Update the current mouse position and delta
+    this.current_.mouseX += movementX;
+    this.current_.mouseY += movementY;
+    this.current_.mouseXDelta = movementX;
+    this.current_.mouseYDelta = movementY;
 
-    this.current_.mouseXDelta = this.current_.mouseX - this.previous_.mouseX;
-    this.current_.mouseYDelta = this.current_.mouseY - this.previous_.mouseY;
+    // Store the current state for the next update
+    this.previous_ = {...this.current_};
   }
 
   onMouseDown_(e) {
@@ -123,8 +156,6 @@ class FirstPersonCamera {
     this.headBobActive_ = false;
     this.headBobTimer_ = 0;
     this.objects_ = objects;
-    //this.path_ = [new THREE.Vector3(0, 2, 0), new THREE.Vector3(10, 2, -10), new THREE.Vector3(10, 2, 10), new THREE.Vector3(-10, 2, 10), new THREE.Vector3(-10, 2, -10)]; // Add your path points here
-    //this.pathIndex_ = 0;
   }
 
   update(timeElapsedS) {
@@ -190,18 +221,7 @@ class FirstPersonCamera {
     const left = new THREE.Vector3(-1, 0, 0);
     left.applyQuaternion(qx);
     left.multiplyScalar(strafeVelocity * timeElapsedS * 10);
-    /*
-    // Calculate new position
-    const newPosition = this.translation_.clone().add(forward).add(left);
-
-    // Check if new position is inside the box
-    if ((newPosition.x > -20 && newPosition.x < -5 || newPosition.x > 5 && newPosition.x < 20) &&
-      newPosition.y > 0 && newPosition.y < 4 &&
-      (newPosition.z > -20 && newPosition.z < -5 || newPosition.z > 5 && newPosition.z < 20)) {
-    // New position is inside the box, don't update position
-    return;
-    }
-    */
+    
     this.translation_.add(forward);
     this.translation_.add(left);
 
@@ -217,7 +237,7 @@ class FirstPersonCamera {
     if (this.translation_.x > roomDimensions.maxX) {
       this.translation_.x = roomDimensions.maxX
     }
-    if (this.translation_.z < roomDimensions.minsdwZ) {
+    if (this.translation_.z < roomDimensions.minZ) {
       this.translation_.z = roomDimensions.minZ
     }
     if (this.translation_.z > roomDimensions.maxZ) {
