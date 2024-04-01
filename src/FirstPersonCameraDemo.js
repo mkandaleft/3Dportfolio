5ba9d85cc64ce29224f4d8ef50ca2074858eaa78
mkandaleft <markandaleft@hotmail.com>
2024-03-31 22:36:10 -0400

    Loading screen almost works but loads you onto of app

--- src/FirstPersonCameraDemo.js
+++ src/FirstPersonCameraDemo.js
@@ -24,7 +24,7 @@ class FirstPersonCameraDemo {
         this.raf_();
         this.onWindowResize_();
         resolve();
-      }, 5000);
+      }, 2000);
     });
   }
 
--- src/main.js
+++ src/main.js
@@ -3,10 +3,7 @@ import FirstPersonCameraDemo from './FirstPersonCameraDemo.js'
 
 let _APP = null;
 
-window.addEventListener('DOMContentLoaded', () => {
-  _APP = new FirstPersonCameraDemo();
-});
-
+// Wait for a click and display the loading screen.
 document.getElementById('start-button').addEventListener('click', function() {
   document.getElementById('overlay').style.display = 'none';
   document.getElementById('loading').style.display = 'flex';
@@ -18,18 +15,3 @@ document.getElementById('start-button').addEventListener('click', function() {
     document.getElementById('app').style.display = 'block';
   });
 });
-
-// // JavaScript
-// document.getElementById('start-button').addEventListener('click', function() {
-//   document.getElementById('overlay').style.display = 'none';
-//   document.getElementById('loading').style.display = 'flex';
-
-//   // Start loading your 3D models here. This is just a placeholder.
-//   loadModels().then(function() {
-//     document.getElementById('loading').style.display = 'none';
-//     document.getElementById('app').style.display = 'block';
-
-//     // Start your 3D application here.
-//     _APP = new FirstPersonCameraDemo();
-//   });
-// });
