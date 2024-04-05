53132795328f3fd39e2491a990ae4b75ec44807c
mkandaleft <markandaleft@hotmail.com>
2024-04-05 10:01:01 -0400

    return buttons are responsive but do not resetView()

--- src/main.js
+++ src/main.js
@@ -13,5 +13,25 @@ document.getElementById('start-button').addEventListener('click', function() {
   _APP.initialize_().then(function() {
     document.getElementById('loading').style.display = 'none';
     document.getElementById('app').style.display = 'block';
+
+    function domContentLoadedCallback() {
+      var resetViewButtons = document.querySelectorAll(".resetView");
+      resetViewButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+          if (_APP && _APP.fpsCamera_) {
+            _APP.fpsCamera_.resetView();
+            console.log("View Reset");
+          } else {
+            console.log("The _APP or _APP.fpsCamera_ is not defined.");
+          }
+        });
+      });
+    }
+    // Verifies if loading is done before attaching listiners
+    if (document.readyState === 'loading') {
+      document.addEventListener("DOMContentLoaded", domContentLoadedCallback);
+    } else {
+      domContentLoadedCallback();
+    }
   });
 });
