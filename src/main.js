8b776b21c6e20b614741e16dec4a3e592e161878
mkandaleft <markandaleft@hotmail.com>
2025-02-23 20:14:50 -0500

    Added statement to check if user is on mobile

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
 import '../style.css'
 import FirstPersonCameraDemo from './FirstPersonCameraDemo.js'
 
+function isMobileDevice() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
 // Create a global variable to hold the 3D application
 let _APP = null;
 
 // Listen for a click on the start button to display the loading screen
 document.getElementById('start-button').addEventListener('click', function(event) {
+
+  if (isMobileDevice()) {
+    // If on a mobile device, display a message
+    alert('This application is not available on mobile devices. Please use a desktop browser.');
+    return;
+  }
 
   document.body.requestPointerLock = document.body.requestPointerLock || document.body.mozRequestPointerLock;
   document.body.requestPointerLock();
